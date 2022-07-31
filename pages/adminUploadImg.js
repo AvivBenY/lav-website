2c7f19f1600e3af081593fcd65fb9ca58c0a191c
AvivBenY <benyedidia123@gmail.com>
2022-07-31 11:28:10 +0300

    bug fix for deployment

--- pages/adminUploadImg.js
+++ pages/adminUploadImg.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import UploadImg from '../components/admin_cmp/UploadImg'
+import UploadImg from '../components/adminCmp/UploadImg'
 
 export default function adminUploadImg() {
   return (
