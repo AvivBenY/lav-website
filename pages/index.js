48e28256b5afc3a0848fbbbec1c8c149974819e7
AvivBenY <benyedidia123@gmail.com>
2022-07-31 15:15:24 +0300

    nr2 font change

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
-import settings from "../setting/setting-development";
 
 export default function Home() {
 
