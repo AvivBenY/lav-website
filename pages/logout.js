f5e3345e04819f15148ebcea495c05146da9e75e
AvivBenY <benyedidia123@gmail.com>
2022-08-01 10:57:39 +0300

    logout session bug fix

--- pages/logout.js
+++ pages/logout.js
@@ -1,12 +1,15 @@
 import { signOut } from "next-auth/react"
-import Link from "next/link"
+import styles from '../styles/SignOut.module.css'
+import { useSession } from "next-auth/react"
 
 export default function logout(){
 
-return(
+    const { data: session } = useSession();
     
-<button onClick={() => signOut()}>Sign out</button>
-)
-}
-
-// signOut({ callbackUrl: process.env.NEXT_PUBLIC_SERVER_URL })}>Sign out</button></Link>
+return(
+    <div>
+        <p>{session.user.name}</p>
+        <button className={styles.signOutBtn} onClick={() => 
+        signOut({ callbackUrl: process.env.NEXT_PUBLIC_SERVER_URL })}>Sign out</button>
+    </div>
+)}
