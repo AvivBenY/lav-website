15b9ad8039ac4516bf69dc95b220c537a102c93b
AvivBenY <benyedidia123@gmail.com>
2022-08-03 13:41:05 +0300

    font test .15

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,20 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import url from "https://fonts.googleapis.com/css2?family=Open+Sans&display=swap";
+// import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+// export default function Document() {
+//   return (
+//     <Html>
+//       <Head>
+//         <link rel="preconnect" href="https://fonts.googleapis.com" />
+//         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+//         <link
+//           href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap"
+//           rel="stylesheet"
+//         />
+//       </Head>
+//       <body>
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
