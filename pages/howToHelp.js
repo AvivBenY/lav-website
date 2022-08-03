40fb3c84ecc056c53b5b7116f15b7e3366923f0f
AvivBenY <benyedidia123@gmail.com>
2022-08-03 13:50:17 +0300

    howToHelp cmp creation

--- pages/howToHelp.js
+++ pages/howToHelp.js
@@ -1,32 +1,6 @@
-import { useState } from 'react'
-import AddVolunteer from '../components/AddVolunteer'
-import styles from '../styles/EventInformation.module.css'
+import React from "react";
+import HowToHelp from "../components/HowToHelp";
 
-export default function HowToHelp() {
-    //CHANGE TO USESTATE FOR FORM TO WORK    
-    const [toggleForm, setToggleForm] = useState(false)
-    return (
-        <div className={styles.mainDiv}>
-            <div className={styles.header}>הצטרפו למשפחה!</div>
-            <div className={styles.infoDiv}>
-                <div className={styles.enEventInfo}>
-                    <p>Visit Us</p>
-                    <p>Jerusalem: <br /> Tuesday</p>
-                    <p>Mahne Yehuda Market <br /> 20:00 - 23:00</p>
-                    <p>Ramat-Gan: <br /> Thursday</p>
-                    <p>Rambam Squar <br /> 20:00 - 23:00</p>
-                </div>
-                <div className={styles.hebEventInfo}>
-                    <p>זמני חלוקות</p>
-                    <p>ירושלים: <br /> יום שלישי</p>
-                    <p> שוק מחנה יהודה <br /> 20:00 - 23:00</p>
-                    <p>:מרכז <br /> יום חמישי</p>
-                    <p>כיכר רמבם/אורדע רמת גן <br /> 20:00 - 23:00</p>
-                </div>                
-            </div>
-            <button className={styles.toggleBtn} onClick={() => setToggleForm(!toggleForm)}>צרו איתי קשר !</button>
-            {toggleForm === true ? <AddVolunteer /> : null}
-        </div>
-    )
+export default function howToHelp() {
+  return <HowToHelp />;
 }
-
