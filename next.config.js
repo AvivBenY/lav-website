73e8b8cae4512b0889c5fc477966b0a5ec026380
AvivBenY <benyedidia123@gmail.com>
2022-08-09 15:06:05 +0300

    font disabled from next.config font 30.

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
-const { DB_PASS, DB_USER, DB_HOST, DB_NAME } = process.env
+const { DB_PASS, DB_USER, DB_HOST, DB_NAME } = process.env;
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    DB_PASS, DB_USER, DB_HOST, DB_NAME
+    DB_PASS,
+    DB_USER,
+    DB_HOST,
+    DB_NAME,
   },
   images: {
-    domains: [
-      'res.cloudinary.com'
-    ],
+    domains: ["res.cloudinary.com"],
   },
-}
+};
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+  nextConfig,
+  optimizeFonts: false,
+};
