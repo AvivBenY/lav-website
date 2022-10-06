38f781f8a5078db528b35cb290260f79a6497a76
AvivBenY <benyedidia123@gmail.com>
2022-10-06 18:47:39 +0300

    design update

--- components/BottomContact.js
+++ components/BottomContact.js
@@ -1,15 +1,21 @@
 import Image from "next/image";
-import React from 'react'
-import styles from '../styles/Home.module.css'
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 export default function BottomContact() {
-  const contact = `ביחד ולמעלה!`
+  const contact = `!יחד ולמעלה`;
   return (
     <footer className={styles.footer}>
       <div className={styles.logo}>
-        <Image src="/logo.png" alt="ל.א.ו לוגו" width={60} height={70} />
+        <Image
+          src="/logo.png"
+          alt="ל.א.ו לוגו"
+          width={60}
+          height={70}
+          className={styles.BottomLogo}
+        />
         <p>{contact}</p>
       </div>
     </footer>
-  )
+  );
 }
--- components/DropDownMenu.js
+++ components/DropDownMenu.js
@@ -1,55 +1,80 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import styles from '../styles/Header.module.css'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import styles from "../styles/Header.module.css";
 import { useSession } from "next-auth/react";
 import { useState } from "react";
 
-export default function MyLink(){
+export default function MyLink() {
+  const [subHeader, setSubHeader] = useState("");
+  const { data: session, status: loading } = useSession();
 
-    const [subHeader, setSubHeader] = useState("");
-    const { data: session, status: loading } = useSession();
+  return (
+    <div className={styles.dropdown}>
+      <button className={styles.dropbtn}>תפריט</button>
+      <div className={styles.content}>
+        <Link href="/">
+          <a onClick={() => setSubHeader("דף הבית")} className={styles.logoBtn}>
+            <Image src="/logo.png" alt="" width={50} height={50} />
+          </a>
+        </Link>
+        <Link href="/about">
+          <a
+            onClick={() => setSubHeader("אודות הארגון")}
+            className={styles.navBtn}
+          >
+            אודות
+          </a>
+        </Link>
 
-return (
-<div className={styles.dropdown}>
-  <button className={styles.dropbtn}>תפריט</button>
-  <div className={styles.content}>
-  <Link href="/"><a onClick={() => setSubHeader('דף הבית')} className={styles.logoBtn}>
-          <Image src="/logo.png" alt="" width={50} height={50} />
-        </a>
+        <Link href="/howToHelp">
+          <a
+            onClick={() => setSubHeader("איך אפשר לעזור")}
+            className={styles.navBtn}
+          >
+            אני רוצה לעזור
+          </a>
         </Link>
-        <Link href="/about"><a onClick={() => setSubHeader('אודות הארגון')} className={styles.navBtn}>
-          אודות
-        </a>
+        <Link href="/gallery">
+          <a onClick={() => setSubHeader("גלריה")} className={styles.navBtn}>
+            גלריה
+          </a>
         </Link>
-        
-        <Link href="/howToHelp"><a onClick={() => setSubHeader('איך אפשר לעזור')} className={styles.navBtn}>
-          אני רוצה לעזור
-        </a>
-        </Link>
-        <Link href="/gallery"><a onClick={() => setSubHeader('גלריה')} className={styles.navBtn}>
-          גלריה
-        </a>
-        </Link>
-        <Link href="/contactUs"><a onClick={() => setSubHeader('צור קשר')} className={styles.navBtn}>
-          צור קשר
-        </a>
+        <Link href="/contactUs">
+          <a onClick={() => setSubHeader("צור קשר")} className={styles.navBtn}>
+            צור קשר
+          </a>
         </Link>
         {session && (
-          <Link href="/addressBook"><a onClick={() => setSubHeader('פרטי משפחות')} className={styles.navBtn}>פרטי משפחות</a>
+          <Link href="/addressBook">
+            <a
+              onClick={() => setSubHeader("פרטי משפחות")}
+              className={styles.navBtn}
+            >
+              פרטי משפחות
+            </a>
           </Link>
         )}
-        {!session ?
-          <Link href="/login"><a onClick={() => setSubHeader('כניסת משתמש')} className={styles.navBtn}>
-            כניסת משתמש
-          </a>
-          </Link> :
-          <Link href="/logout"><a onClick={() => setSubHeader('התנתקות')} className={styles.navBtn}>
-            התנתקות
-          </a>
+        {!session ? (
+          <Link href="/login">
+            <a
+              onClick={() => setSubHeader("כניסת משתמש")}
+              className={styles.navBtn}
+            >
+              כניסת משתמש
+            </a>
           </Link>
-        }      
+        ) : (
+          <Link href="/logout">
+            <a
+              onClick={() => setSubHeader("התנתקות")}
+              className={styles.navBtn}
+            >
+              התנתקות
+            </a>
+          </Link>
+        )}
+      </div>
     </div>
-</div>
-)
+  );
 }
